c95266d3f0f95d9f4b6e6fa3dc7881db561c032a
rilieo <rilesdou@gmail.com>
2024-04-06 15:16:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Project 7 - *Task List*
+
+Submitted by: **Riley Dou**
+
+**Task List** is an app that allows you to organize your tasks. You can create new tasks, edit existing tasks, and mark them as complete to see your progress! You can also view each task in a calendar, so you can easily see which tasks are set for which date.
+
+Time spent: **3** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] App displays a list of tasks
+- [x] Users can add tasks to the list
+- [x] Session persists when application is closed and relaunched (tasks dont get deleted when closing app) 
+  - [x] Note: You have to quit the app, not minimize it, in order to see the persistence.
+- [x] Tasks can be deleted
+- [x] Users have a calendar view via navigation controller that displays tasks    
+
+
+The following **additional** features are implemented:
+
+- [x] Tasks can be toggled completed
+- [x] User can edit tasks by tapping on the task in the feed view
+- [ ] Be able to view detais of the task in the calendar view
+- [ ] For tasks that have a location, add a map view
+
+## Video Walkthrough
+
+[Demo Pt.1](https://imgur.com/52XH0aj)
+[Demo Pt.2](https://imgur.com/DTilFWz)
+
+## Notes
+
+A challenge was implementing the edit task functionality. When clicking on a task to edit, it returned a new uuid instead of the uuid of the existing task; thus, duplicates of the task were added instead of replacing that task.
+
+## License
+
+    Copyright [2024] [Riley Dou]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- ios101-project7-task/Task.swift
+++ ios101-project7-task/Task.swift
@@ -5,28 +5,29 @@
 import UIKit
 
 // The Task model
-struct Task {
-
+struct Task : Decodable, Encodable {
+    
     // The task's title
     var title: String
-
+    
     // An optional note
     var note: String?
-
+    
     // The due date by which the task should be completed
     var dueDate: Date
-
+    
     // Initialize a new task
     // `note` and `dueDate` properties have default values provided if none are passed into the init by the caller.
-    init(title: String, note: String? = nil, dueDate: Date = Date()) {
+    init(id: String, title: String, note: String? = nil, dueDate: Date = Date()) {
+        self.id = id
         self.title = title
         self.note = note
         self.dueDate = dueDate
     }
-
+    
     // A boolean to determine if the task has been completed. Defaults to `false`
     var isComplete: Bool = false {
-
+        
         // Any time a task is completed, update the completedDate accordingly.
         didSet {
             if isComplete {
@@ -37,33 +38,49 @@ struct Task {
             }
         }
     }
-
+    
     // The date the task was completed
     // private(set) means this property can only be set from within this struct, but read from anywhere (i.e. public)
     private(set) var completedDate: Date?
-
+    
     // The date the task was created
     // This property is set as the current date whenever the task is initially created.
     let createdDate: Date = Date()
-
+    
     // An id (Universal Unique Identifier) used to identify a task.
-    let id: String = UUID().uuidString
+    let id: String
 }
 
 // MARK: - Task + UserDefaults
 extension Task {
-
+    
+    static var tasksKey : String = "tasksKey"
 
     // Given an array of tasks, encodes them to data and saves to UserDefaults.
     static func save(_ tasks: [Task]) {
 
         // TODO: Save the array of tasks
+        
+        let defaults = UserDefaults.standard
+        
+        let encodedData = try! JSONEncoder().encode(tasks)
+        
+        defaults.set(encodedData, forKey: tasksKey)
+        
     }
 
     // Retrieve an array of saved tasks from UserDefaults.
     static func getTasks() -> [Task] {
         
         // TODO: Get the array of saved tasks from UserDefaults
+        
+        let defaults = UserDefaults.standard
+        
+        if let currTasks = defaults.data(forKey: tasksKey) {
+            let decodedData = try! JSONDecoder().decode([Task].self, from: currTasks)
+            
+            return decodedData
+        }
 
         return [] // 👈 replace with returned saved tasks
     }
@@ -72,5 +89,28 @@ extension Task {
     func save() {
 
         // TODO: Save the current task
+        
+        var tasks = Task.getTasks()
+        var idx : Int?
+        
+        print(self)
+        
+        for (index, elem) in tasks.enumerated() {
+            if (elem.id == self.id) {
+                idx = index
+                break
+            }
+        }
+        
+        if let index = idx {
+            tasks.remove(at: index)
+            tasks.insert(self, at: index)
+        }
+        else {
+            tasks.append(self)
+        }
+        
+        Task.save(tasks)
+        
     }
 }
